ba9f3716bd6b6b64db9d8ede5ac36886daa769b5
MoonChildFi <Lucaseduarte6@gmail.com>
2025-08-30 02:33:05 -0300

    PL-2.9

---
+++ Pale Luna™ Terminal Edition - Todas as versões/Pale-Luna V2.9/Pale Luna™/assets/credits.bat
@@ -0,0 +1,24 @@
+@echo off
+MODE CON COLS=80 LINES=20
+setlocal enabledelayedexpansion
+    :: Conteúdo dos créditos
+    echo                             Pale Luna: Terminal Edition
+    echo.
+    echo                                    Direcao
+    echo                                  Lucas Eduardo
+    echo.
+    echo                                   Programacao
+    echo                                  Lucas Eduardo
+    echo                                Kayc Felix (dicas)
+    echo.
+    echo                                Artes Promocionais
+    echo                                  Lucas Eduardo
+    echo.
+    echo                                    Musica
+    echo                                  Ryan Creep
+    echo.
+    echo                             Agradecimentos Especiais
+    echo                                Todos os jogadores!
+    echo.
+    echo                                     FIM
+pause
